44be34437f2f23eeba79579d16e35dc13a4e16f0
Krishnaprasanth <dhawansaran14346@gmail.com>
2026-02-21 01:18:06 +0530

    feat: add play games sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,15 +41,18 @@ android {
         val undoLastMoveRewardedAdId = localProperties.getProperty("UNDO_LAST_MOVE_REWARDED_AD_ID") ?: "ca-app-pub-3940256099942544/5224354917"
         val undoRewardedAdId = localProperties.getProperty("UNDO_REWARDED_AD_ID") ?: "ca-app-pub-3940256099942544/5224354917"
         // val unityGameId = localProperties.getProperty("UNITY_GAME_ID") ?: "6028752"
+        val playgameAppId = localProperties.getProperty("PLAYGAME_APP_ID") ?: "";
 
         buildConfigField("String", "ADMOB_APP_ID", "\"$admobAppId\"")
         buildConfigField("String", "APP_BOTTOM_BANNER_AD_ID", "\"$appBottomBannerId\"")
         buildConfigField("String", "GAMEOVER_RESET_INTERSTITIAL_AD_ID", "\"$gameoverResetInterstitialId\"")
         buildConfigField("String", "UNDO_LAST_MOVE_REWARDED_AD_ID", "\"$undoLastMoveRewardedAdId\"")
         buildConfigField("String", "UNDO_REWARDED_AD_ID", "\"$undoRewardedAdId\"")
+        buildConfigField("String", "PLAY_GAMES_GAME_ID", "\"$playgameAppId\"")
 
         // Inject App ID into Manifest
         manifestPlaceholders["ADMOB_APP_ID"] = admobAppId
+        manifestPlaceholders["PLAY_GAMES_APP_ID"] = playgameAppId
 
         // Enable Native Debug Symbols for Play Store
         ndk {
@@ -116,4 +119,6 @@ dependencies {
 
     // Ads
     implementation(libs.play.services.ads)
+
+    implementation(libs.play.services.games.v2)
 }
